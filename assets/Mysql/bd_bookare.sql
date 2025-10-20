30be13e6b8b43e04d0d48ddfad1ac3e63b6af04d
Cleberson <cleber4557@gmail.com>
2025-10-19 22:04:45 -0300

    O banco tem a coluna Uf agora
    Insert com todas as UF

--- assets/Mysql/bd_bookare.sql
+++ assets/Mysql/bd_bookare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11/10/2025 às 03:36
+-- Tempo de geração: 20/10/2025 às 02:59
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -40,7 +40,8 @@ CREATE TABLE `tb_autor` (
 
 INSERT INTO `tb_autor` (`id_autor`, `autor`) VALUES
 (1, 'Arthur Conan Doyle'),
-(2, 'Agatha Christie');
+(2, 'Agatha Christie'),
+(3, 'Edgar Allan Poe');
 
 -- --------------------------------------------------------
 
@@ -70,6 +71,14 @@ CREATE TABLE `tb_contato` (
   `fixo` varchar(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Despejando dados para a tabela `tb_contato`
+--
+
+INSERT INTO `tb_contato` (`id_contato`, `id_usuario`, `ddd`, `celular`, `fixo`) VALUES
+(6, 9, '', '969295557', ''),
+(7, 10, '36', '969295758', '');
+
 -- --------------------------------------------------------
 
 --
@@ -87,7 +96,8 @@ CREATE TABLE `tb_editora` (
 
 INSERT INTO `tb_editora` (`id_editora`, `editora`) VALUES
 (1, 'Principis'),
-(2, 'Globo Livros');
+(2, 'Globo Livros'),
+(3, 'Lebooks Editora');
 
 -- --------------------------------------------------------
 
@@ -103,9 +113,17 @@ CREATE TABLE `tb_endereco` (
   `complemento` varchar(20) NOT NULL,
   `bairro` varchar(20) NOT NULL,
   `cidade` varchar(20) NOT NULL,
-  `id_estado` int(11) NOT NULL,
+  `id_estado` int(11) DEFAULT NULL,
   `cep` varchar(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Despejando dados para a tabela `tb_endereco`
+--
+
+INSERT INTO `tb_endereco` (`id_endereco`, `id_usuario`, `logradouro`, `numero`, `complemento`, `bairro`, `cidade`, `id_estado`, `cep`) VALUES
+(6, 9, 'Rua Gonçalves de Lima', '22', 'Fundos', 'Recanto Verde do Sol', '', NULL, '08382100'),
+(7, 10, 'Rua dos oitos', '22', 'Fundos', 'Itaquera', 'São Paulo', 25, '07292100');
 
 -- --------------------------------------------------------
 
@@ -115,8 +133,42 @@ CREATE TABLE `tb_endereco` (
 
 CREATE TABLE `tb_estado` (
   `id_estado` int(11) NOT NULL,
-  `estado` varchar(20) NOT NULL
+  `estado` varchar(20) NOT NULL,
+  `uf` char(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Despejando dados para a tabela `tb_estado`
+--
+
+INSERT INTO `tb_estado` (`id_estado`, `estado`, `uf`) VALUES
+(1, 'Acre', 'AC'),
+(2, 'Alagoas', 'AL'),
+(3, 'Amapá', 'AP'),
+(4, 'Amazonas', 'AM'),
+(5, 'Bahia', 'BA'),
+(6, 'Ceará', 'CE'),
+(7, 'Distrito Federal', 'DF'),
+(8, 'Espírito Santo', 'ES'),
+(9, 'Goiás', 'GO'),
+(10, 'Maranhão', 'MA'),
+(11, 'Mato Grosso', 'MT'),
+(12, 'Mato Grosso do Sul', 'MS'),
+(13, 'Minas Gerais', 'MG'),
+(14, 'Pará', 'PA'),
+(15, 'Paraíba', 'PB'),
+(16, 'Paraná', 'PR'),
+(17, 'Pernambuco', 'PE'),
+(18, 'Piauí', 'PI'),
+(19, 'Rio de Janeiro', 'RJ'),
+(20, 'Rio Grande do Norte', 'RN'),
+(21, 'Rio Grande do Sul', 'RS'),
+(22, 'Rondônia', 'RO'),
+(23, 'Roraima', 'RR'),
+(24, 'Santa Catarina', 'SC'),
+(25, 'São Paulo', 'SP'),
+(26, 'Sergipe', 'SE'),
+(27, 'Tocantins', 'TO');
 
 -- --------------------------------------------------------
 
@@ -134,7 +186,8 @@ CREATE TABLE `tb_genero` (
 --
 
 INSERT INTO `tb_genero` (`id_genero`, `genero`) VALUES
-(1, 'Fiction');
+(1, 'Fiction'),
+(2, 'Juvenile Fiction');
 
 -- --------------------------------------------------------
 
@@ -153,7 +206,8 @@ CREATE TABLE `tb_idioma` (
 --
 
 INSERT INTO `tb_idioma` (`id_idioma`, `codigo_idioma`, `nome`) VALUES
-(1, 'pt', '');
+(1, 'pt', ''),
+(2, 'es', '');
 
 -- --------------------------------------------------------
 
@@ -182,7 +236,8 @@ CREATE TABLE `tb_livro` (
 --
 
 INSERT INTO `tb_livro` (`id_livro`, `id_usuario`, `isbn`, `nome_livro`, `id_livro_imagem`, `id_genero`, `id_autor`, `id_editora`, `id_idioma`, `ano_pub_livro`, `sinopse_livro`, `data_add_livro`, `estado_conservacao_livro`) VALUES
-(2, 1, '', 'E não sobrou nenhum', 1, 1, 2, 2, 1, 2014, 'Uma ilha misteriosa, um poema infantil, dez soldadinhos de porcelana e muito suspense são os ingredientes com que Agatha Christie constrói seu romance mais importante. Na ilha do Soldado, antiga propriedade de um milionário norte-americano, dez pessoas sem nenhuma ligação aparente são confrontadas por uma voz misteriosa com fatos marcantes de seus passados. Convidados pelo misterioso mr. Owen, nenhum dos presentes tem muita certeza de por que estão ali, a despeito de conjecturas pouco convincentes que os leva a crer que passariam um agradável período de descanso em mordomia. Entretanto, já na primeira noite, o mistério e o suspense se abatem sobre eles e, num instante, todos são suspeitos, todos são vítimas e todos são culpados. É neste clima de tensão e desconforto que as mortes inexplicáveis começam e, sem comunicação com o continente devido a uma forte tempestade, a estadia transforma-se em um pesadelo. Todos se perguntam: quem é o misterioso anfitrião, mr. Owen? Existe mais alguém na ilha? O assassino pode ser um dos convidados? Que mente ardilosa teria preparado um crime tão complexo? E, sobretudo, por quê? São essas e outras perguntas que o leitor será desafiado a resolver neste fabuloso romance de Agatha Christie, que envolve os espíritos mais perspicazes num complexo emaranhado de situações, lembranças e acusações na busca deste sagaz assassino. Medo, confinamento e angústia: que o leitor descubra por si mesmo porque E não sobrou nenhum foi eleito o melhor romance policial de todos os tempos.', '2025-10-10 23:09:00', 'Novo');
+(2, 1, '', 'E não sobrou nenhum', 1, 1, 2, 2, 1, 2014, 'Uma ilha misteriosa, um poema infantil, dez soldadinhos de porcelana e muito suspense são os ingredientes com que Agatha Christie constrói seu romance mais importante. Na ilha do Soldado, antiga propriedade de um milionário norte-americano, dez pessoas sem nenhuma ligação aparente são confrontadas por uma voz misteriosa com fatos marcantes de seus passados. Convidados pelo misterioso mr. Owen, nenhum dos presentes tem muita certeza de por que estão ali, a despeito de conjecturas pouco convincentes que os leva a crer que passariam um agradável período de descanso em mordomia. Entretanto, já na primeira noite, o mistério e o suspense se abatem sobre eles e, num instante, todos são suspeitos, todos são vítimas e todos são culpados. É neste clima de tensão e desconforto que as mortes inexplicáveis começam e, sem comunicação com o continente devido a uma forte tempestade, a estadia transforma-se em um pesadelo. Todos se perguntam: quem é o misterioso anfitrião, mr. Owen? Existe mais alguém na ilha? O assassino pode ser um dos convidados? Que mente ardilosa teria preparado um crime tão complexo? E, sobretudo, por quê? São essas e outras perguntas que o leitor será desafiado a resolver neste fabuloso romance de Agatha Christie, que envolve os espíritos mais perspicazes num complexo emaranhado de situações, lembranças e acusações na busca deste sagaz assassino. Medo, confinamento e angústia: que o leitor descubra por si mesmo porque E não sobrou nenhum foi eleito o melhor romance policial de todos os tempos.', '2025-10-10 23:09:00', 'Novo'),
+(4, 9, '', 'O POÇO E O PÊNDULO', 3, 1, 3, 3, 1, 2022, 'Edgar Allan Poe (1809-1849) foi um poeta, escritor, crítico literário e editor norte-americano. Poe foi um extraordinário contista e escreveu memoráveis contos de mistério, terror e policiais, sempre com seu estilo único e pioneiro. Nesta edição o leitor terá o privilégio de conhecer três contos que, sem dúvida, figuram entre os melhores e mais conhecidos de Edgar Allan Poe; Prepare-se para mergulhar numa sucessão de três histórias de tirar o fôlego e que por um bom tempo restarão em sua memória. São elas: O Poço e o Pêndulo; Morella e O Coração Delator.', '2025-10-13 18:42:32', 'Novo');
 
 -- --------------------------------------------------------
 
@@ -215,7 +270,8 @@ CREATE TABLE `tb_livro_imagem` (
 --
 
 INSERT INTO `tb_livro_imagem` (`id_livro_imagem`, `id_livro`, `tipo_imagem`, `caminho_imagem`, `ordem_imagem`, `dataAdd_imagem`) VALUES
-(1, 2, 'capa', 'uploads/livros/2/capa_1760137740_68e9920cda072.jpg', 0, '2025-10-10 23:09:00');
+(1, 2, 'capa', 'uploads/livros/2/capa_1760137740_68e9920cda072.jpg', 0, '2025-10-10 23:09:00'),
+(3, 4, 'capa', 'uploads/livros/4/capa_1760380952_68ed48183792b.jpg', 0, '2025-10-13 18:42:33');
 
 -- --------------------------------------------------------
 
@@ -252,7 +308,10 @@ CREATE TABLE `tb_usuario` (
 --
 
 INSERT INTO `tb_usuario` (`id_usuario`, `nome_usuario`, `email_usuario`, `senha_usuario`, `status_usuario`, `foto_usuario`, `criado_em`) VALUES
-(1, 'Cleberson', 'cleber4557@gmail.com', '$2y$10$YXMBc1extbO2Flh3iZ5MxOpP/o2Kk8UZ9oUWeSTgoOn2IXN.R5/Om', 0, 'uploads/usuarios/1/d16c3a6197db66722a8ef27a768cec26.jpg', '2025-10-06 21:57:30');
+(1, 'Cleberson', 'cleber4557@gmail.com', '$2y$10$YXMBc1extbO2Flh3iZ5MxOpP/o2Kk8UZ9oUWeSTgoOn2IXN.R5/Om', 0, 'uploads/usuarios/1/3a0417695478a52ad15b1bc93d8a0e5d.jpg', '2025-10-06 21:57:30'),
+(3, 'jordan', 'jo@gmail.com', '$2y$10$u/NaTL/xQLEik83S4cpEG.AR289Aef2Vx3.Ls2l5k04.I1hWs7Q5W', 0, '', '2025-10-13 13:46:49'),
+(9, 'pedro', 'per@gmail.com', '$2y$10$Nwf2YiyXH4QJTXovunn7C.mSRFmcjqWkVf7KiZze4fYArPFfd6sVO', 0, 'uploads/usuarios/9/1496c4afdb5d923168794a7c201ec20b.jpg', '2025-10-13 18:24:24'),
+(10, 'Cleiton', 'cleiton@gmail.com', '$2y$10$eCQ8WSzie399Cyc/ChRk3eqXQzXQfx2DaCIpvJy/Uf9KtPePpD2UK', 0, 'uploads/usuarios/10/c219237665969ca9519dada4c907708c.jpg', '2025-10-15 21:14:16');
 
 --
 -- Índices para tabelas despejadas
@@ -360,7 +419,7 @@ ALTER TABLE `tb_usuario`
 -- AUTO_INCREMENT de tabela `tb_autor`
 --
 ALTER TABLE `tb_autor`
-  MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `tb_avaliacao`
@@ -372,49 +431,49 @@ ALTER TABLE `tb_avaliacao`
 -- AUTO_INCREMENT de tabela `tb_contato`
 --
 ALTER TABLE `tb_contato`
-  MODIFY `id_contato` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_contato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `tb_editora`
 --
 ALTER TABLE `tb_editora`
-  MODIFY `id_editora` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_editora` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `tb_endereco`
 --
 ALTER TABLE `tb_endereco`
-  MODIFY `id_endereco` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_endereco` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `tb_estado`
 --
 ALTER TABLE `tb_estado`
-  MODIFY `id_estado` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_estado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de tabela `tb_genero`
 --
 ALTER TABLE `tb_genero`
-  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `tb_idioma`
 --
 ALTER TABLE `tb_idioma`
-  MODIFY `id_idioma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_idioma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `tb_livro`
 --
 ALTER TABLE `tb_livro`
-  MODIFY `id_livro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_livro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `tb_livro_imagem`
 --
 ALTER TABLE `tb_livro_imagem`
-  MODIFY `id_livro_imagem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_livro_imagem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `tb_troca`
@@ -426,7 +485,7 @@ ALTER TABLE `tb_troca`
 -- AUTO_INCREMENT de tabela `tb_usuario`
 --
 ALTER TABLE `tb_usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restrições para tabelas despejadas
