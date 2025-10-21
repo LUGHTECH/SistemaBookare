0a61d069957a5d91b1a885821cf41e3e5b227e84
Cleberson <cleber4557@gmail.com>
2025-10-20 21:15:51 -0300

    Informações completas de cada livro
    Estilizado, falta apenas alguns ajustes

---
+++ assets/js/modalLivro.js
@@ -0,0 +1,29 @@
+
+
+function abrirModalLivro(livro) {
+    
+        document.getElementById("tituloLivro").textContent = livro.nome_livro || "Sem título";
+        document.getElementById("autorLivro").textContent = livro.nome_autor || "Desconhecido";
+        document.getElementById("generoLivro").textContent = livro.nome_genero || "—";
+        document.getElementById("idiomaLivro").textContent = livro.idioma || "—";
+        document.getElementById("anoLivro").textContent = livro.ano_pub_livro || "—";
+        document.getElementById("editoraLivro").textContent = livro.nome_editora || "—";
+        document.getElementById("sinopseLivro").textContent = livro.sinopse_livro || "Sem sinopse disponível";
+
+
+
+    const capa = document.getElementById("capaLivro");
+    capa.src = livro.capa ? `/SistemaBookare/includes/dashboard/tabViews/${livro.capa}` : "../assets/img/defaultBook.png";
+
+    const modal = document.getElementById('modalLivro')
+    modal.style.opacity = "1";
+    modal.style.visibility = "visible";
+}
+
+function fecharModal() {
+
+    document.querySelectorAll(".modal, .modal2, .modal3").forEach(modal => {
+        modal.style.opacity = "0";
+        modal.style.visibility = "hidden";
+    });
+}
--- assets/js/refresh.js
+++ assets/js/refresh.js
@@ -1,3 +1,5 @@
 function refresh(){
 location.reload();
 }
+
+
